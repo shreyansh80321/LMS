8f613a8ff6306067f1f948b2cf8c7df82217ab06
SHREYANSH80321 <SIBERSUPER1122@GMAIL.com>
2025-09-13 03:32:46 +0530

    fixing

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -11,12 +11,13 @@ exports.login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
-    res.cookie("token", token, {
-      httpOnly: true, 
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, 
-    });
+   res.cookie("token", token, {
+     httpOnly: true,
+     secure: true, // required for cross-site cookies
+     sameSite: "none", // allow frontend on different domain
+     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+   });
+
     res.status(200).json({
       message: "Login successful",
       user: { id: user._id, email: user.email, name: user.name },
